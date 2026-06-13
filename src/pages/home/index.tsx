dfdd3dd5c7d3c19d530d3a21b5b392337c568fb1
Johnathon Weaver <weaver123_johnathon@hotmail.com>
2026-06-14 00:14:06 +0800

    style: mobile-friendly layout, header/footer polish, hex background, github icon, tagline

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,11 @@ import './styles.css';
 
 const HomePage = () => {
 	return (
-		<div className="home-logo-card">
-			<img src={Logo} alt="Stumble" />
+		<div className="home-content">
+			<div className="home-logo-card">
+				<img src={Logo} alt="Stumble" />
+			</div>
+			<p className="home-tagline">Evolution through Revolution</p>
 		</div>
 	);
 };

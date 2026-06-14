4724450fa43ea01fda2ef75d8405ee6a36dfabc9
Johnathon Weaver <weaver123_johnathon@hotmail.com>
2026-06-14 19:10:22 +0800

    style: clean up hex background, inline svg, redundant code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,10 @@
-import Logo from '../../assets/images/logo.svg';
-
 import './styles.css';
 
 const HomePage = () => {
 	return (
 		<div className="home-content">
 			<div className="home-logo-card">
-				<img src={Logo} alt="Stumble" />
+				<img src="/assets/logo.svg" alt="Stumble" />
 			</div>
 			<p className="home-tagline">Evolution through Revolution</p>
 		</div>
